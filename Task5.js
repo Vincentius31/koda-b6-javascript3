571d9a5633fb30f2b75acad77e9c4572ebda47a6
Vincentius Dwi Prasetyo <62978348+Vincentius31@users.noreply.github.com>
2025-12-31 13:35:25 +0700

    Add files via upload

---
+++ Task5.js
@@ -0,0 +1,47 @@
+const data = [
+    { name: "Koda", wait: 5 },
+    { name: "Mamet", wait: 4 },
+    { name: "James", wait: 7 },
+    { name: "Kevin", wait: 6 },
+    { name: "Mike", wait: 8 }
+]
+
+console.log("Menunggu Antrian...")
+
+function tunggu(waktu) {
+    return new Promise((resolve, reject) => {
+        if (waktu < 0) {
+            reject("Waktu tidak boleh 0")
+        }
+
+        setTimeout(() => {
+            console.log("Menunggu Pesanan...")
+            resolve()
+        }, waktu * 1000)
+    })
+}
+
+function pesananSiap(nama) {
+    return new Promise(resolve => {
+        console.log(`Halo ${nama}, pesanan kamu sudah siap`)
+        resolve()
+    })
+}
+
+function tungguPesanan(data) {
+    let hasil = Promise.resolve()
+
+    for (let i = 0; i < data.length; i++) {
+        hasil = hasil.then(() => tunggu(data[i].wait)).then(() => pesananSiap(data[i].name))
+    }
+
+    return hasil
+}
+
+tungguPesanan(data)
+    .then(() => {
+        console.log("Semua pesanan selesai")
+    })
+    .catch(error => {
+        console.log("Terjadi error:", error)
+    })
